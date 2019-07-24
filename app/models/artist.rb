9fb99d02dcc9f5920de90302ab5245d3fd321bcb
Michael Fox <michael.c.fox.ii@gmail.com>
2019-07-24 16:37:26 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,3 @@
 class Artist < ActiveRecord::Base
-  has_many :songs 
-  has_many :genres, through: :songs
-  
-  def get_genre_of_first_song
-    self.songs.first.genre
-  end
 
-  def song_count
-    self.songs.length 
-  end
-  
-  def genre_count
-    self.genre.length
-  end
 end
