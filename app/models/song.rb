9bfae86b188af90b40ce4c5fd939a5fced3d5c29
Michael Fox <michael.c.fox.ii@gmail.com>
2019-07-24 16:38:20 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,13 +1,3 @@
 class Song < ActiveRecord::Base
-  belongs_to :artist 
-  belongs_to :genre 
-  
-  def get_genre_name
-    self.genre.name 
-  end
-  
-  def drake_made_this 
-    
-  end
-  
+
 end
