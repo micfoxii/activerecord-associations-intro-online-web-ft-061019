32266daec36f13fb95256f9398a6e6519454f486
Michael Fox <michael.c.fox.ii@gmail.com>
2019-07-24 16:37:32 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,17 +1,3 @@
 class Genre < ActiveRecord::Base
-  has_many :songs 
-  has_many :artists, through: :songs 
-  
-  def song_count 
-    self.songs.length 
-  end
-  
-  def artist_count 
-    self.artists.length 
-  end
-  
-  def all_artist_names
-    # self.artists.map {|artist| artist.name}
-    self.artists.pluck{:name}
-  end
+
 end
